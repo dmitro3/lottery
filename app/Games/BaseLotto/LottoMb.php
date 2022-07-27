31e1e12aa2e14ac76d99366fb2b4c248e59bcde1
trinh toanh <kythuat.tech5s@gmail.com>
2022-07-27 15:49:58 +0700

    update code

--- app/Games/BaseLotto/LottoMb.php
+++ app/Games/BaseLotto/LottoMb.php
@@ -2,7 +2,10 @@
 
 namespace App\Games\BaseLotto;
 
-
+use App\Models\Games\LottoMb\GameLottoMbPlayRecord;
+use App\Models\Games\LottoMb\GameLottoMbPlayType;
+use App\Models\Games\LottoMb\GameLottoMbPlayUserBet;
+use App\Models\Games\LottoMb\GameLottoMbTableResult;
 
 class LottoMb extends BaseLotto
 {
@@ -12,14 +15,14 @@ class LottoMb extends BaseLotto
     }
     function getGameRecord()
     {
-        return GameLottoPlayRecord::class;
+        return GameLottoMbPlayRecord::class;
     }
     function getGameTableResult()
     {
-        return GameLottoTableResult::class;
+        return GameLottoMbTableResult::class;
     }
     function getGameUserBet()
     {
-        return GameLottoPlayUserBet::class;
+        return GameLottoMbPlayUserBet::class;
     }
 }
