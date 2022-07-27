f35ba412dc8f6a87a35b7c117a944089db014d28
Vu Tuan Hung <hungvt1804@gmail.com>
2022-07-27 17:32:56 +0700

    update thoi gian choi lotto

--- app/Games/LottoMb/Enums/Config.php
+++ app/Games/LottoMb/Enums/Config.php
@@ -6,6 +6,6 @@ use App\Games\Plinko\Enums\BaseEnum;
 
 class Config extends BaseEnum
 {
-    public const LAST_POINT_TO_BET  = 3600;
-    public const NUMBER_TIME_TO_CHECK = 3600;
+    public const LAST_POINT_TO_BET  = 3500;
+    public const NUMBER_TIME_TO_CHECK = 3500;
 }
