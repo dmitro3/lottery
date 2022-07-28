f8ed63cdebc516f7638f99aec65f4a5b13ac2fcd
unknown <tuanhung.tech5s@gmail.com>
2022-07-28 09:14:37 +0700

    t20220727

---
+++ app/Console/Commands/LottoMb/CrawlSample.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Console\Commands\LottoMb;
+
+use App\Models\Games\Lotto\GameLottoPlayType;
+use App\Models\Games\LottoMb\GameLottoMbPlayRecord;
+use App\Models\Games\LottoMb\GameLottoMbPlayType;
+use App\Models\Games\LottoMb\GameLottoMbTableResult;
+use crawlmodule\loterie\XoSoSources\KetQuaNet;
+use Illuminate\Console\Command;
+
+class CrawlSample extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'lottomb:crawl-sample {id}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Crawl Sample';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+
+        $this->info("Start " . $this->description);
+        $kq = new KetQuaNet(GameLottoMbPlayRecord::find($this->argument('id')));
+        $table = $kq->getResults();
+        foreach ($table as $key => $row) {
+            foreach ($row as $col) {
+                $CLASS_GAME_TABLE_RESULT = GameLottoMbTableResult::class;
+                $item = new $CLASS_GAME_TABLE_RESULT();
+                $item->game_lotto_play_record_id = $this->argument('id');
+                $item->type_prize = $key + 1;
+                $item->value = $col;
+                $item->created_at = now();
+                $item->updated_at = now();
+                $item->save();
+            }
+        }
+        $this->info("End " . $this->description);
+    }
+}
--- app/Models/Games/LottoMb/GameLottoMbPlayType.php
+++ app/Models/Games/LottoMb/GameLottoMbPlayType.php
@@ -15,4 +15,80 @@ class GameLottoMbPlayType extends GameLottoPlayType
     {
         return $this->hasMany(GameLottoMbPlayRecord::class, 'game_lotto_play_type_id');
     }
+    protected function renderGameRecord()
+    {
+        $yesterday = now()->addDays(-1);
+        $existYesterday =  $this->isExistItemByDate($yesterday);
+        if (!$existYesterday) {
+            $this->generateGameByTime($yesterday);
+        }
+
+        $today = now();
+        $existToday =  $this->isExistItemByDate($today);
+        if (!$existToday) {
+            $this->generateGameByTime($today);
+        }
+
+        $tomorrow = now()->addDays(1);
+        $existTomorrow = $this->isExistItemByDate($tomorrow);
+        if (!$existTomorrow) {
+            $this->generateGameByTime($tomorrow);
+        }
+    }
+    protected function generateGameByTime($timeAnchor)
+    {
+        $startDate = clone $timeAnchor;
+        $endDate = clone $timeAnchor;
+        $endDate->addDays(1);
+
+        $startDate->hour(19);
+        $startDate->minute(0);
+        $startDate->second(0);
+
+        $endDate->hour(19);
+        $endDate->minute(0);
+        $endDate->second(0);
+
+
+        $timeStampStart = $startDate->timestamp;
+
+
+        $currentMinuteRange = $this->seconds / 60;
+        $day = $timeAnchor->day;
+        $month = $timeAnchor->month;
+        $year = $timeAnchor->year;
+        $count = 1;
+        $dataInsert = [];
+        for ($i = 0; $i < 24; $i++) {
+            for ($j = 0; $j < 60; $j++) {
+                if (($i * 60 + $j) % $currentMinuteRange == 0) {
+                    $dataAdd = [];
+                    $dataAdd['id'] = (int)($year . $startDate->format('m') . $startDate->format('d') . $this->id . sprintf('%s%04s', '', $count));
+                    $dataAdd['day'] = $day;
+                    $dataAdd['month'] = $month;
+                    $dataAdd['year'] = $year;
+                    $dataAdd['minute'] = $j;
+                    $dataAdd['hour'] = $i;
+                    $dataAdd['game_lotto_play_type_id'] = $this->id;
+                    $dataAdd['is_end'] = 0;
+                    $dataAdd['admin_init'] = 0;
+                    $dataAdd['created_at'] = now();
+                    $dataAdd['updated_at'] = now();
+                    $dataAdd['start_time'] = $timeStampStart;
+                    $endTime = $endDate->timestamp;
+                    $dataAdd['end_time'] = $endTime;
+                    $timeStampStart = $endTime;
+                    array_push($dataInsert, $dataAdd);
+                    $count++;
+                }
+            }
+            if ($count % 200 == 0) {
+                $this->getGameLottoRecordClass()::insert($dataInsert);
+                $dataInsert = [];
+            }
+        }
+        if (count($dataInsert) > 0) {
+            $this->getGameLottoRecordClass()::insert($dataInsert);
+        }
+    }
 }
